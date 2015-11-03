9536461f9a9275c8fe1f05c255f37cbc877577ce
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-03 12:25:38 -0700

    added id's and html elments to use find and replace for p2

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 		<meta	charset="utf-8" />
 		<title>JavaScript Assn #1</title>
 		<script type="text/javascript" src="javascript/js-events.js" ></script>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+		<!-- customized style sheet -->
+		<link rel="stylesheet" href="css/javascript-practice.css" />
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
 	</head>
 
 	<body>
@@ -13,9 +26,20 @@
 			The Deliverator belongs to an elite order, a hallowed subcategory. He ' s got esprit up to here. Right now, he is preparing to carry out his third mission of the night. His uniform is black as activated charcoal, filtering the very light out of the air. A bullet will bounce off its arachnofiber weave like a wren
 			hitting a patio door, but excess perspiration wafts through it like a breeze through a freshly napalmed forest, Where his body has bony extremities, the suit has sintered armorgel: feels like gritty jello, protects like a stack of telephone books.
 		</p>
+
 		<p id="p2">
 			When they gave him the job, they gave him a gun. The Deliverator never deals in cash, but someone might come after him anyway -- might want his car, or his cargo. The gun is tiny, acm-styled, lightweight, the kind of gun a fashion designer would carry; it fires teensy darts that fly at five times the velocity of an SR-71 spy plane, and when you get done using it, you have to plug it into the cigarette lighter, because it runs on electricity.
 		</p>
+		<a class="btn btn-primary" id="popUp" onclick="popUp();">Find and Replace a word in the above paragraph</a>
+		<div id="FindAndReplace">
+			<h3>Find and Replace</h3>
+			<input type="text" id="find" value="Word to be replaced">
+			<input type="text" id="replace" value="Replace it with...">
+			<div id="closePopUp" onclick="closeFindAndReplace();">Close</div>
+		</div>
+		<br />
+		<br />
+
 		<p id="p3">
 			The Deliverator never pulled that gun in anger, or in fear. He pulled it once in Gila Highlands. Some punks in Gila Highlands, a fancy Burbclave, wanted themselves a delivery, and they didn't want to pay for it. Thought they would impress the Deliverator with a baseball bat. The Deliverator took out his gun, centered its laser doohickey on that poised Louisville Slugger, fired it. The
 			recoil was immense, as though the weapon had blown up in his hand. The middle third of the baseball bat turned into a column of burning sawdust accelerating in all directions like a bursting star. Punk ended up holding this bat handle with milky smoke pouring out the end. Stupid look on his face. Didn't get nothing but trouble from the Deliverator.
--- javascript/js-events.js
+++ javascript/js-events.js
@@ -5,5 +5,12 @@ function clickEvent() {
 	var string = document.getElementById("p1").innerHTML;
 	var newString = string.toUpperCase(string);
 	document.getElementById("p1").innerText=newString;
+}
 
+function popUp() {
+	document.getElementById("FindAndReplace").style.display = "block";
+}
+
+function closeFindAndReplace() {
+	document.getElementById("FindAndReplace").style.display = "none";
 }
