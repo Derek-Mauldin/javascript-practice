156d1498891b2b7e3b589effe1a7c4fa16b96a51
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-03 13:52:45 -0700

    added comments for findAndReplace

--- javascript/js-events.js
+++ javascript/js-events.js
@@ -7,22 +7,35 @@ function clickEvent() {
 	document.getElementById("p1").innerText=newString;
 }
 
+/**
+ * opens the FindAndReplace <div>
+ **/
 function openFindAndReplace() {
 	document.getElementById("FindAndReplace").style.display = "block";
 }
 
+/**
+ * closes the FindAndReplace <div>
+ **/
 function closeFindAndReplace() {
 	document.getElementById("FindAndReplace").style.display = "none";
 }
 
+/**
+ * function to find a word in a string from the "find" text box
+ * and replace it with the string in the "replace" text box
+ **/
 function findAndReplace() {
+
+	// get input from the document
 	var string = document.getElementById("p2").innerHTML;
 	var findWord = document.getElementById("find").value;
 	var replaceWord = document.getElementById("replace").value;
 
+	// setup the regular expression and execute the replacement
 	var regExpInput = new RegExp(findWord, "g");
 	newString = string.replace(regExpInput, replaceWord );
 
+	// put the newString back into p2
 	document.getElementById("p2").innerHTML = newString;
-
 }
